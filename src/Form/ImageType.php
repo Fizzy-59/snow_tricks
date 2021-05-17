60ec9de6837d2c3bbf8c01070a6c5a5ed17a3602
Romain Devillez <rom.dev@live.fr>
2021-05-17 17:55:04 +0200

    work on front

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -18,6 +18,9 @@ class ImageType extends AbstractType
     {
         $builder
             ->add('caption', TextType::class, ["label" => "Description of image",
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => ['class' => 'form-control'],
+                'row_attr' => ['class' => 'form-group'],
                 'constraints' => new Length(['min' => 5, 'max' => 700])
             ])
             ->add('file', FileType::class, ["label" => "File of image"])
